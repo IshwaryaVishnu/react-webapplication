e2f9acdfff185756980e87416b1476826fe2e38c
Iswarya Devi Gomathi Sankar <ishu27.v@gmail.com>
2023-03-29 11:05:24 +0200

    updated

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom/client";
 
-ReactDOM.render(
-  <h1>Hello, React!</h1>,
-  document.getElementById('root')
-);
+const greetingMessage =<div>Hello React!</div>;
+console.log(">>>>>>> Hello React!");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(greetingMessage);
